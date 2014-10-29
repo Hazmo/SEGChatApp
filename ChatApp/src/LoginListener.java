7477086cd3221abbd6282850d7ed9a86675bebe0
Codrin Gidei <Justme94c@yahoo.com>
2014-10-29 14:39:39 +0000

    added Settings functionality and configured User Class

--- ChatApp/src/LoginListener.java
+++ ChatApp/src/LoginListener.java
@@ -1,5 +1,7 @@
 package src;
+
 import javax.swing.*;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
@@ -9,84 +11,96 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 public class LoginListener implements ActionListener {
-	
-	private JTextField studentIDField;
-	private JPasswordField passField;
-	
-        /** Parametric constructor providing the text field 
-	 *     and the password field from the login
-	 */
-	public LoginListener(JTextField studentIDField, JPasswordField passField) {
-		this.studentIDField = studentIDField;
-		this.passField = passField;
-	}
 
-	public void actionPerformed(ActionEvent event) {
-		File users = new File("users.csv");
-		int count = 0;
-		Scanner sc;
-		try {
-			sc = new Scanner(users);
-			while(sc.hasNextLine()) {
-				count++;
-				sc.nextLine();
-			}
-			
-			sc.close();
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		}
-		
-		String[][] userData = new String[count][3];
-		
-		String csvFile = "users.csv";
-		BufferedReader br = null;
-		String line = "";
-		String csvSplitBy = ",";
-		
-		try {
-			br = new BufferedReader(new FileReader(csvFile));
-			int count2 = 0;
-			while((line = br.readLine()) != null) {
-				String[] temp = line.split(csvSplitBy);
-				userData[count2][0] = temp[0];
-				userData[count2][1] = temp[1];
-				userData[count2][2] = temp[3];
-				count2++;
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if(br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		
-		String user = studentIDField.getText();
-		String pass = String.valueOf(passField.getPassword());
-		
-		MessageDigest md;
-		String encrypted_pass;
+    private final JTextField studentIDField;
+    private final JPasswordField passField;
 
-            String passString = new String(passField.getPassword());
-			boolean login_true = false;
-			for(int i = 0; i < count; i++) {
-				if((user.equals(userData[i][1])) && (passString.equals(userData[i][2]))) {
-					login_true = true;
-				}
-			} 
-			if(login_true == true) {
-                System.out.println("Correct login");
-                new StudentGUI();
-			} else {
-				JOptionPane.showMessageDialog(null, "No such login");
-			}
-	}
+    /**
+     * Parametric constructor providing the text field and the password field from the login
+     */
+    public LoginListener(final JTextField studentIDField, final JPasswordField passField) {
+        this.studentIDField = studentIDField;
+        this.passField = passField;
+    }
 
+    @Override
+    public void actionPerformed(final ActionEvent event) {
+        final File users = new File("users.csv");
+        int count = 0;
+        Scanner sc;
+        try {
+            sc = new Scanner(users);
+            while (sc.hasNextLine()) {
+                count++;
+                sc.nextLine();
+            }
+
+            sc.close();
+        }
+        catch (final FileNotFoundException e1) {
+            e1.printStackTrace();
+        }
+
+        final String[][] userData = new String[count][6];
+
+        final String csvFile = "users.csv";
+        BufferedReader br = null;
+        String line = "";
+        final String csvSplitBy = ",";
+
+        try {
+            br = new BufferedReader(new FileReader(csvFile));
+            int count2 = 0;
+            while ((line = br.readLine()) != null) {
+                final String[] temp = line.split(csvSplitBy);
+                userData[count2][0] = temp[0];
+                userData[count2][1] = temp[1];
+                userData[count2][2] = temp[2];
+                userData[count2][3] = temp[3];
+                userData[count2][4] = temp[4];
+                userData[count2][5] = temp[5];
+                count2++;
+            }
+        }
+        catch (final FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (final IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (br != null) {
+                try {
+                    br.close();
+                }
+                catch (final IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        final String user = this.studentIDField.getText();
+        final String passString = new String(this.passField.getPassword());
+
+        final String[] userLoginData = new String[6];
+
+        boolean login_true = false;
+        for (int i = 0; i < count; i++) {
+            if ((user.equals(userData[i][1])) && (passString.equals(userData[i][3]))) {
+                login_true = true;
+
+                for (int j = 0; j < 6; j++) {
+                    userLoginData[j] = userData[i][j];
+                }
+                break;
+            }
+        }
+        if (login_true == true) {
+            System.out.println("Correct login");
+            new StudentGUI(new UserClass(userLoginData));
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "No such login");
+        }
+    }
 }
--- ChatApp/src/UserClass.java
+++ ChatApp/src/UserClass.java
@@ -1,17 +1,72 @@
 package src;
-public class UserClass {
-    String userName;
-    boolean isAdmin;
 
-    public UserClass(String userName, boolean isAdmin) {
-        this.userName = userName;
-        this.isAdmin = isAdmin;
+public class UserClass {
+    String[] userData;
+
+    public UserClass(final String[] userData) {
+        this.userData = userData;
     }
 
     @Override
     public String toString() {
-        return userName;
+        return this.userData[2];
     }
-    
-    
+
+    public String getID() {
+        return this.userData[1];
+    }
+
+    public boolean isAdmin() {
+        if (this.userData[0].equals("0")) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    public String getName() {
+        return this.userData[2];
+    }
+
+    public String getQuestion() {
+        return this.userData[4];
+    }
+
+    public String getAnswer() {
+        return this.userData[5];
+    }
+
+    public String getPassword() {
+        return this.userData[3];
+    }
+
+    public void setName(final String name) {
+        this.userData[2] = name;
+    }
+
+    public void setID(final String ID) {
+        this.userData[1] = ID;
+    }
+
+    public void setAdmin(final boolean isAdmin) {
+        if (isAdmin) {
+            this.userData[0] = "1";
+        }
+        else {
+            this.userData[0] = "0";
+        }
+    }
+
+    public void setQuestion(final String question) {
+        this.userData[4] = question;
+    }
+
+    public void setAnswer(final String answer) {
+        this.userData[5] = answer;
+    }
+
+    public void setPassword(final String password) {
+        this.userData[3] = password;
+    }
 }
